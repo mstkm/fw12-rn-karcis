9a514f81369e4ca3333ed1651529247b1f50730c
mstkm <99125333+mstkm@users.noreply.github.com>
2023-01-27 17:09:01 +0700

    add env

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {BACKEND_URL} from '@env';
+import {URL_BACKEND} from '@env';
 
 const http = token => {
   const headers = {};
@@ -7,7 +7,7 @@ const http = token => {
     headers.authorization = 'Bearer ' + token;
   }
   const instance = axios.create({
-    baseURL: BACKEND_URL,
+    baseURL: URL_BACKEND,
     headers,
   });
   return instance;
